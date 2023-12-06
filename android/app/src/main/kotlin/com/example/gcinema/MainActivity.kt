2e5461d8bad3f417aee45af191e66826201ac317
Geraldo Quende <43283373+gquende@users.noreply.github.com>
2023-12-06 15:59:48 +0100

    FEAT: Init

---
+++ android/app/src/main/kotlin/com/example/gcinema/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gcinema
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:gcinema/pages/home.dart';
+
+void main() {
+//  runApp(const MyApp());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
